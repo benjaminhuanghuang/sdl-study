8de9e24c4ffd34f6911543b7358ecd1d21bc9720
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-02-04 23:19:25 -0800

    scene end

--- SDLShooter/src/SceneEnd.h
+++ SDLShooter/src/SceneEnd.h
@@ -5,5 +5,11 @@
 
 class SceneEnd : public Scene
 {
+public:
+    void init() override;
+    void update(float deltaTime) override;
+    void render() override;
+    void clean() override;
+    void handleEvent(SDL_Event *event) override;
 };
 #endif // SCENEEND_H
