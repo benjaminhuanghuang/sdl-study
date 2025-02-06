ef773b54ea19144962b36e04508202eb8a39e8c8
benjanmin huang <benjaminhuanghuang@gmail.com>
2025-02-05 23:02:35 -0800

    input-remove chinese

--- SDLShooter/src/SceneEnd.cpp
+++ SDLShooter/src/SceneEnd.cpp
@@ -4,6 +4,11 @@
 
 void SceneEnd::init()
 {
+    // start input
+    if (!SDL_IsTextInputActive())
+    {
+        SDL_StartTextInput();
+    }
 }
 
 void SceneEnd::update(float deltaTime)
@@ -27,6 +32,29 @@ void SceneEnd::clean()
 
 void SceneEnd::handleEvent(SDL_Event *event)
 {
+    if (isTyping)
+    {
+        if (event->type == SDL_TEXTINPUT)
+        {
+            playerName += event->text.text;
+        }
+
+        if (event->type == SDL_KEYDOWN)
+        {
+            if (event->key.keysym.scancode == SDL_SCANCODE_RETURN)
+            {
+                isTyping = false;
+                SDL_StopTextInput();
+            }
+            else if (event->key.keysym.scancode == SDL_SCANCODE_BACKSPACE)
+            {
+                removeLastUTF8Char(playerName);
+            }
+        }
+    }
+    else
+    {
+    }
 }
 
 void SceneEnd::renderPhase1()
@@ -39,7 +67,31 @@ void SceneEnd::renderPhase1()
     game.renderTextCentered(gameOver, 0.4, true);
     game.renderTextCentered(scoreText, 0.2, false);
     game.renderTextCentered(instructionText, 0.6, false);
+
+    if (playerName != "")
+    {
+        std::string playerNameText = "Player: " + playerName;
+        game.renderTextCentered(playerNameText, 0.3, false);
+    }
 }
 void SceneEnd::renderPhase2()
 {
+}
+
+void SceneEnd::removeLastUTF8Char(std::string &str)
+{
+    if (str.empty())
+    {
+        return;
+    }
+    auto lastchar = str.back();
+    if ((lastchar & 0b10000000) == 0b10000000) // is last part of chinese
+    {
+        str.pop_back();
+        while ((str.back() & 0b11000000) != 0b11000000) // remove all chinese parts utile the first part
+        {
+            str.pop_back();
+        }
+    }
+    str.pop_back();
 }
--- SDLShooter/src/SceneEnd.h
+++ SDLShooter/src/SceneEnd.h
@@ -2,7 +2,7 @@
 #define SCENEEND_H
 
 #include "Scene.h"
-
+#include <string>
 class SceneEnd : public Scene
 {
 public:
@@ -13,8 +13,10 @@ public:
     void handleEvent(SDL_Event *event) override;
 
 private:
+    std::string playerName = "";
     bool isTyping = true;
     void renderPhase1();
     void renderPhase2();
+    void removeLastUTF8Char(std::string &str);
 };
 #endif // SCENEEND_H
